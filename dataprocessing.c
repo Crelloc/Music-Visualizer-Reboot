433ff03374b4d6e301429dc46c18a82246984824
thomas <thomastdt@gmail.com>
2016-08-15 03:09:18 -0700

    dataprocessing header and source files

---
+++ dataprocessing.c
@@ -0,0 +1,7 @@
+#include "dataprocessing.h"
+
+
+void processWAVFile(Uint8* wavStart, Uint32 wavLength, Visualizer_Pkg_ptr vis_pkg_ptr){
+
+	printf("%s\n", vis_pkg_ptr->filename);
+}
---
+++ dataprocessing.h
@@ -0,0 +1,10 @@
+#ifndef DATAPROCESSING_H
+#define DATAPROCESSING_H
+
+#include "audioInformation.h"
+#include <SDL2/SDL.h>
+
+
+void processWAVFile(Uint8* wavStart, Uint32 wavLength, Visualizer_Pkg_ptr vis_pkg_ptr);
+
+#endif
