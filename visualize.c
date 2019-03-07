1c014444b3caa73db48c328c4f0543e1774b8633
thomas <thomastdt@gmail.com>
2019-03-06 23:06:21 -0800

    visualize.c: improved organization
    Signed-off-by: thomas <thomastdt@gmail.com>

--- visualize.c
+++ visualize.c
@@ -52,103 +52,136 @@ static int InitializePackage(SDL_AudioSpec* wavSpec,  Uint8* wavStart, Uint32 wa
     return ret;
 }
 
-static void InitializeVariables(struct Visualizer_Pkg* vis_pkg, SDL_AudioSpec have,
-                         SDL_AudioDeviceID device)
+static SDL_AudioSpec* Verify_Audio_Spec(SDL_AudioSpec* wavSpec, SDL_AudioSpec have)
 {
-    int sizeof_packet, totalpackets, frame_size, i, ch;
-    struct AudioData* audio;
 
-    vis_pkg->device = device;
-    vis_pkg->setupDFT = setupDFTForSound;
-
-    SDL_AudioSpec* wavSpec = GetSDL_AudioSpec(vis_pkg);
-    //initialize function ptr
-    if(wavSpec->format != have.format)
+    if(wavSpec->format != have.format){
+        printf("original sample format: %d\n"
+        "new sample format: %d\n", wavSpec->format, have.format);
         wavSpec->format = have.format;
-
-    vis_pkg->bitsize = (int)SDL_AUDIO_BITSIZE(wavSpec->format);
-
-    if(wavSpec->channels != have.channels)
+    }
+    if(wavSpec->channels != have.channels){
+        printf("original sample channels: %d\n"
+        "new sample channels: %d\n", wavSpec->channels, have.channels);
         wavSpec->channels = have.channels;
-
+    }
     if(wavSpec->samples != have.samples ){
         printf("original sample size: %d\n"
         "new sample size: %d\n", wavSpec->samples, have.samples);
         wavSpec->samples = have.samples;
     }
+    return wavSpec;
+}
+
+static struct FFTWop* allocate_FFTWop(int channels, int frame_size)
+{
+    int i;
+    struct FFTWop* fftw_ptr = malloc(channels*sizeof(struct FFTWop));
+    if(!fftw_ptr) exit(EXIT_FAILURE);
+    //allocating space for dft operations
+    for(i=0; i<channels; ++i){
+        fftw_ptr[i].in = fftw_malloc(sizeof(fftw_complex) * frame_size);
+        if(!fftw_ptr[i].in) exit(EXIT_FAILURE);
+        fftw_ptr[i].out = fftw_malloc(sizeof(fftw_complex) * frame_size);
+        if(!fftw_ptr[i].out) exit(EXIT_FAILURE);
+        fftw_ptr[i].index = i;
+    }
+    return fftw_ptr;
+}
+
+
+static struct FFTW_Results*  allocate_FFTW_Results(int channels, int totalpackets)
+{
+    int i, ch;
+    struct FFTW_Results* FFTW_Results_ptr;
+
+    FFTW_Results_ptr = malloc(totalpackets * sizeof(struct FFTW_Results));
+    if(!(FFTW_Results_ptr)) exit(EXIT_FAILURE);;
+
+    for (i = 0; i < totalpackets; ++i){
+        //for peak results
+        FFTW_Results_ptr[i].peakfreq = malloc(channels*sizeof(double));
+
+        if(!FFTW_Results_ptr[i].peakfreq) exit(EXIT_FAILURE);
+
+        FFTW_Results_ptr[i].peakpower = malloc(channels*sizeof(double));
+
+        if(!FFTW_Results_ptr[i].peakpower) exit(EXIT_FAILURE);
+        //for power spectrum (i.e. a double matrix) of
+        //N BUCKETS that represent a frequency range
+        FFTW_Results_ptr[i].peakmagMatrix = malloc(channels*sizeof(double));
+        if(!FFTW_Results_ptr[i].peakmagMatrix) exit(EXIT_FAILURE);
+        for(ch = 0; ch < channels ; ++ch){
+            FFTW_Results_ptr[i].peakmagMatrix[ch] = malloc(BUCKETS*sizeof(double));
+            if(!FFTW_Results_ptr[i].peakmagMatrix[ch]) exit(EXIT_FAILURE);
+        }
+    }
+    return FFTW_Results_ptr;
+}
+
+static int Get_totalpackets(struct Visualizer_Pkg* vis_pkg, SDL_AudioSpec* wavSpec)
+{
+    int sizeof_packet;
+    struct AudioData* audio;
 
     //size of samples in bytes?
     sizeof_packet =  vis_pkg->bitsize / 8 ;
-    sizeof_packet *=wavSpec->channels;
+    sizeof_packet *= wavSpec->channels;
     sizeof_packet *= wavSpec->samples;
+    printf("buffer size per packet [bytes]: %d\n", sizeof_packet);
 
     //find the total number of packets
     //wavLength is the size of audio data in bytes
     audio = GetAudioData(vis_pkg);
+    return (int)ceil((float)audio->wavLength/sizeof_packet);
+}
 
-    totalpackets= (int)ceil((float)audio->wavLength/sizeof_packet);
-    vis_pkg->total_packets = totalpackets;
+
+static int Get_total_frames(struct Visualizer_Pkg* vis_pkg, int channels)
+{
+    struct AudioData* audio;
+    audio = GetAudioData(vis_pkg);
+    return audio->wavLength /= ((vis_pkg->bitsize/8) * channels);
+}
+
+static double (*set_audio_sample_function(int bitsize))(Uint8 *, SDL_AudioFormat)
+{
+
+    return  (bitsize == 8 ) ? Get8bitAudioSample
+        :   (bitsize == 16) ? Get16bitAudioSample
+        :   (bitsize == 32) ? Get32bitAudioSample
+        :   NULL;
+}
+
+
+static void InitializeVariables(struct Visualizer_Pkg* vis_pkg, SDL_AudioSpec have,
+                         SDL_AudioDeviceID device)
+{
+    SDL_AudioSpec* wavSpec;
+
+    wavSpec           = Verify_Audio_Spec(GetSDL_AudioSpec(vis_pkg), have);
+    vis_pkg->bitsize  = (int)SDL_AUDIO_BITSIZE(wavSpec->format);
+    vis_pkg->GetAudioSample = set_audio_sample_function(vis_pkg->bitsize);
+    assert(vis_pkg->GetAudioSample != NULL);
+
+    vis_pkg->device   = device;
+    vis_pkg->setupDFT = setupDFTForSound;
+    vis_pkg->total_packets = Get_totalpackets(vis_pkg, wavSpec);
+
 
     //A frame can consist of N channels
-    frame_size = wavSpec->samples;
-    vis_pkg->frame_size = frame_size;
-    vis_pkg->total_frames = audio->wavLength;
-    vis_pkg->total_frames /= ((vis_pkg->bitsize/8) * wavSpec->channels);
+    vis_pkg->total_frames = Get_total_frames(vis_pkg, wavSpec->channels);
+    vis_pkg->frame_size = wavSpec->samples;
+
     //FFTW Results for each packet
-    //
+    vis_pkg->FFTW_Results_ptr = allocate_FFTW_Results(wavSpec->channels, vis_pkg->total_packets);
+    //EXIT_IF(ret==-1, device);
+    vis_pkg->fftw_ptr = allocate_FFTWop(wavSpec->channels, vis_pkg->frame_size);
+    //EXIT_IF(ret==-1, device);
 
-    vis_pkg->FFTW_Results_ptr = malloc(totalpackets * sizeof(struct FFTW_Results));
-    EXIT_IF(!vis_pkg->FFTW_Results_ptr, device);
-
-    for (i = 0; i < totalpackets; ++i){
-        //for peak results
-        vis_pkg->FFTW_Results_ptr[i].peakfreq = malloc(wavSpec->channels*sizeof(double));
-
-        EXIT_IF(!vis_pkg->FFTW_Results_ptr[i].peakfreq, device);
-
-        vis_pkg->FFTW_Results_ptr[i].peakpower = malloc(wavSpec->channels*sizeof(double));
-
-        EXIT_IF(!vis_pkg->FFTW_Results_ptr[i].peakpower, device);
-        //for power spectrum (i.e. a double matrix) of
-        //N BUCKETS that represent a frequency range
-        vis_pkg->FFTW_Results_ptr[i].peakmagMatrix = malloc(wavSpec->channels*sizeof(double));
-        EXIT_IF(!vis_pkg->FFTW_Results_ptr[i].peakmagMatrix, device);
-        for(ch = 0; ch < wavSpec->channels ; ++ch){
-            vis_pkg->FFTW_Results_ptr[i].peakmagMatrix[ch] = malloc(BUCKETS*sizeof(double));
-            EXIT_IF(!vis_pkg->FFTW_Results_ptr[i].peakmagMatrix[ch], device);
-        }
-
-    }
-    vis_pkg->fftw_ptr = malloc(wavSpec->channels*sizeof(struct FFTWop));
-    EXIT_IF(!vis_pkg->fftw_ptr, device);
-    //allocating space for dft operations
-    for(i=0; i<wavSpec->channels; ++i){
-        vis_pkg->fftw_ptr[i].in = fftw_malloc(sizeof(fftw_complex) * frame_size);
-        EXIT_IF(!vis_pkg->fftw_ptr[i].in, device);
-        vis_pkg->fftw_ptr[i].out = fftw_malloc(sizeof(fftw_complex) * frame_size);
-        EXIT_IF(!vis_pkg->fftw_ptr[i].out, device);
-        vis_pkg->fftw_ptr[i].index = i;
-    }
-
-    switch(vis_pkg->bitsize){
-        case 8:
-            printf("8 bit data samples\n");
-            vis_pkg->GetAudioSample = Get8bitAudioSample;
-            break;
-        case 16:
-            printf("16 bit data samples\n");
-            vis_pkg->GetAudioSample = Get16bitAudioSample;
-            break;
-        case 32:
-            vis_pkg->GetAudioSample = Get32bitAudioSample;
-            printf("32 bit data samples\n");
-            break;
-        default:
-            break;
-    }
-    printf("total packets: %d\n", totalpackets);
-    printf("buffer size per packet [bytes]: %d\n", sizeof_packet);
-    printf("number of frames per packet: %d\n", frame_size);
+    printf("%d bit data samples\n", vis_pkg->bitsize);
+    printf("total packets: %d\n", vis_pkg->total_packets);
+    printf("number of frames per packet: %d\n", vis_pkg->frame_size);
     printf("total frames: %d\n", vis_pkg->total_frames);
     printf("FILE_PATH: %s\n", vis_pkg->filename);
     printf("\nPress ENTER to continue:\n");
